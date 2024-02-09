35f56d19d7cb00bdaf8f95549a19cad4976abb93
Marcus Carvalho <135276762+gatinhodev@users.noreply.github.com>
2024-02-09 06:20:35 -0300

    fix bugs
    fix: bugs fixed

--- verifycat_api.go
+++ verifycat_api.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"github.com/gatinhodev/verifycat/validate"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"verifycat/validate"
 )
 
 func main() {
 	router := gin.Default()
 
-	// Rota para manipulação de validações
-	router.POST("/validate", validate.ValidateHandler)
+	// Rota para manipulação de validações (aceita GET e POST)
+	router.Any("/validate", validate.ValidateHandler)
 
 	// Iniciar o servidor na porta 8080
 	if err := http.ListenAndServe(":8080", router); err != nil {
